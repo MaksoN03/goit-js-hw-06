1a4088c4b915d05cc0da59a937cb3af26e4ec6a0
Max Gerasymenko <geramaks03@gmail.com>
2023-01-14 18:56:17 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    const { email, password } = evt.currentTarget.elements;
+    if (!email.value || !password.value) {
+        alert('Please, write info');
+    }
+
+    const userEmail = email.value;
+    const userPassword = password.value;
+
+    const formData = {
+        userEmail,
+        userPassword,
+    };
+
+    console.log(formData);
+    formEl.reset();
+}
